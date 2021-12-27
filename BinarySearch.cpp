67d1642d4d583d25967c46682c2246702544e12a
Shabina Siddiqui <siddiquishabina10@gmail.com>
2021-12-27 22:22:17 +0530

    Binary Seach

---
+++ BinarySearch.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+        int n=nums.size();
+        int high=n-1,low=0;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(target==nums[mid]){
+                return mid;
+            }
+            else if(target>nums[mid]){
+                low = mid+1;
+            }
+            else{
+                high=mid-1;
+                
+            }
+        }
+        return -1;
+    }
+};
